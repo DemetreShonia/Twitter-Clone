8e88f1efefb57d312a91f3470fe1e0c039246fd6
Demetre Shonia <shonka.demeloper@gmail.com>
2024-06-20 12:58:14 +0400

    Import Boilerplate assets and other stuff

---
+++ lib/constants/appwrite_constants.dart
@@ -0,0 +1,5 @@
+class AppwriteConstants {
+  static const String dataBaseId = '6673e30b003b4f64b0f2';
+  static const String projectId = '6673decf000428c57041';
+  static const String endPoint = 'https://appwrite.hoppla.autos/v1';
+}
